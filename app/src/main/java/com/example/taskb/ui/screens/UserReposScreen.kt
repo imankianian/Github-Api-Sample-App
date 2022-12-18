ac740836399554785db9d6e01b841fec3f6fc00c
Iman Kianian <iman.kianian98@gmail.com>
2022-12-18 12:37:38 +0330

    Fix UserReposScreen layout

--- app/src/main/java/com/example/taskb/ui/screens/UserReposScreen.kt
+++ app/src/main/java/com/example/taskb/ui/screens/UserReposScreen.kt
@@ -1,23 +1,23 @@
 package com.example.taskb.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.taskb.R
 import com.example.taskb.data.model.Repo
 import com.example.taskb.ui.ReposUiState
-import com.example.taskb.ui.theme.BrilliantAzure
 import com.example.taskb.ui.viewmodel.UserReposViewModel
 
 @Composable
@@ -53,11 +53,15 @@ fun UserReposErrorScreen(message: String) {
 
 @Composable
 fun UserReposListScreen(repos: List<Repo>, onNavigateToDetails: (login: String) -> Unit) {
-    LazyColumn(modifier = Modifier
-        .padding(10.dp)
-        .background(Color.White)) {
+    LazyColumn(modifier = Modifier.background(Color.White)) {
         items(repos) { repo ->
+            Spacer(modifier = Modifier.size(10.dp))
             RepoCard(repo = repo, onNavigateToDetails)
+            Spacer(modifier = Modifier.size(10.dp))
+            Divider(color = MaterialTheme.colorScheme.onSurface.copy(0.05f),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp))
         }
     }
 }
@@ -65,16 +69,23 @@ fun UserReposListScreen(repos: List<Repo>, onNavigateToDetails: (login: String) 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RepoCard(repo: Repo, onNavigateToDetails: (login: String) -> Unit) {
-    Card(onClick = { onNavigateToDetails(repo.htmlUrl) },
-        modifier = Modifier.padding(bottom = 20.dp)) {
+    Card(onClick = { onNavigateToDetails(repo.htmlUrl) }) {
         Column(modifier = Modifier
-            .height(200.dp)
             .fillMaxWidth()
             .background(Color.White)) {
-            RepoName(name = repo.name)
-            RepoUpdateDate(date = repo.lastUpdate)
-            RepoStar(count = repo.stars)
-            RepoLanguage(language = repo.language ?: "Unknown")
+            Row() {
+                Spacer(modifier = Modifier.size(20.dp))
+                Column() {
+                    RepoName(name = repo.name)
+                    RepoUpdateDate(date = repo.lastUpdate)
+                    Spacer(modifier = Modifier.size(5.dp))
+                    Row() {
+                        RepoStar(count = repo.stars)
+                        Spacer(modifier = Modifier.size(15.dp))
+                        RepoLanguage(language = repo.language ?: "Unknown")
+                    }
+                }
+            }
         }
     }
 }
@@ -82,39 +93,49 @@ fun RepoCard(repo: Repo, onNavigateToDetails: (login: String) -> Unit) {
 @Composable
 fun RepoName(name: String) {
     Text(text = name,
-        color = BrilliantAzure,
-        fontSize = 18.sp,
-        textAlign = TextAlign.Start,
-        modifier = Modifier.padding(start = 10.dp)
+        color = Color.Black,
+        fontSize = 17.sp,
+        textAlign = TextAlign.Start
     )
 }
 
 @Composable
 fun RepoUpdateDate(date: String) {
     Text(text = date,
-        color = BrilliantAzure,
-        fontSize = 18.sp,
-        textAlign = TextAlign.Start,
-        modifier = Modifier.padding(start = 10.dp)
+        color = Color.LightGray,
+        fontSize = 10.sp,
+        textAlign = TextAlign.Start
     )
 }
 
 @Composable
 fun RepoStar(count: Int) {
-    Text(text = count.toString(),
-        color = BrilliantAzure,
-        fontSize = 18.sp,
-        textAlign = TextAlign.Start,
-        modifier = Modifier.padding(start = 10.dp)
-    )
+    Row() {
+        Image(painter = painterResource(id = R.drawable.ic_star),
+            modifier = Modifier
+                .width(15.dp)
+                .height(20.dp),
+            contentDescription = "stars")
+        Spacer(modifier = Modifier.size(5.dp))
+        Text(text = count.toString(),
+            color = Color.Gray,
+            fontSize = 15.sp,
+            textAlign = TextAlign.Start
+        )
+    }
 }
 
 @Composable
 fun RepoLanguage(language: String) {
+    Row() {
+        Image(painterResource(id = R.drawable.ic_language),
+        modifier = Modifier.width(15.dp).height(20.dp),
+        contentDescription = "programming language")
+    }
+    Spacer(modifier = Modifier.size(5.dp))
     Text(text = language,
-        color = BrilliantAzure,
-        fontSize = 18.sp,
-        textAlign = TextAlign.Start,
-        modifier = Modifier.padding(start = 10.dp)
+        color = Color.Gray,
+        fontSize = 15.sp,
+        textAlign = TextAlign.Start
     )
 }
